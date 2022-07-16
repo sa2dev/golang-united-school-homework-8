4849783ab78c29c5d54241667a875faecaac5931
Max Reshetnikov <maxim.a.reshetnikov@gmail.com>
2022-07-16 14:46:58 +0400

    Implemments parseArgs

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"os"
 )
 
+type Arguments map[string]string
+
 func Perform(args Arguments, writer io.Writer) error {
+	fmt.Fprint(writer, args)
+
+	return nil
+}
+
+func parseArgs() Arguments {
+	arguments := make(Arguments)
+	idFlag := flag.String("id", "", "Id")
+	itemFlag := flag.String("item", "", "Item")
+	operationFlag := flag.String("operation", "", "Operation")
+	fileNameFlag := flag.String("fileName", "", "File name")
+	flag.Parse()
+	arguments["id"] = *idFlag
+	arguments["item"] = *itemFlag
+	arguments["operation"] = *operationFlag
+	arguments["fileName"] = *fileNameFlag
+
+	return arguments
 }
 
 func main() {
